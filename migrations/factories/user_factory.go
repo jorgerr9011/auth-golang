c1c24be23fe9c458b74a60edfd37bd87d25fa3d1
jorgerr9011 <jorge.rodriguez.rivera@udc.es>
2025-03-07 19:53:06 +0100

    Seeder y Factory de Usuarios.

---
+++ migrations/factories/user_factory.go
@@ -0,0 +1,22 @@
+package factories
+
+import (
+	"fmt"
+	model "jorgerr9011/wiki-golang/internal/model/user"
+	"math/rand"
+	"time"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+// UserFactory genera un usuario falso
+func UserFactory() model.User {
+	return model.User{
+		Name:      faker.Name(),
+		Email:     fmt.Sprintf("%s@example.com", faker.Username()),
+		Password:  fmt.Sprintf("password%d", rand.Intn(1000)), // Simulación de hash
+		Username:  faker.Username(),
+		Phone:     fmt.Sprintf("9%d", rand.Intn(100000000)), // Número de 9 dígitos
+		LastLogin: time.Now().Add(-time.Duration(rand.Intn(1000)) * time.Hour),
+	}
+}
---
+++ migrations/seeders/user_seeder.go
@@ -0,0 +1,20 @@
+package seeders
+
+import (
+	"fmt"
+	"jorgerr9011/wiki-golang/migrations/factories"
+	"log"
+
+	"gorm.io/gorm"
+)
+
+// UserSeeder inserta 10 usuarios en la BD
+func UserSeeder(db *gorm.DB) {
+	for i := 0; i < 100; i++ {
+		user := factories.UserFactory()
+		if err := db.Create(&user).Error; err != nil {
+			log.Fatalf("Error insertando usuario: %v", err)
+		}
+		fmt.Println("✔ Usuario creado:", user.Username, user.Email)
+	}
+}
